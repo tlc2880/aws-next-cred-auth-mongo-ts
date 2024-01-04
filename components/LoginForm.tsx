2da346a2f4c8594403e493dd8c20f6a328edfb6d
tlc2880 <tommylcao@gmail.com>
2024-01-04 04:59:39 -0800

    first commit

---
+++ components/LoginForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function LoginForm() {
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+        <h1>Enter the details</h1>
+      </div>
+    </div>
+  )
+}
