651c72a54a033555b7913d908582818c9d983af4
tlc2880 <tommylcao@gmail.com>
2024-01-09 05:55:02 -0800

    Add nextauth route and LoginForm error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "./Providers";
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -18,7 +19,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="max-w-3xl mx-auto p-4">
-          <div className="mt-8">{children}</div>
+          <div className="mt-8">
+            <AuthProvider>
+              {children}
+            </AuthProvider>
+          </div>
         </div>
       </body>
     </html>
