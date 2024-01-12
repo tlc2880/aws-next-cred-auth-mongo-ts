f3210010ac0e5f8f11910e6de78df68848f9ff3e
tlc2880 <tommylcao@gmail.com>
2024-01-11 17:51:07 -0800

    Add server session

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -46,3 +46,4 @@ const authOptions: AuthOptions = {
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST}
+export default authOptions;
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,12 @@
 import RegisterForm from "@/components/RegisterForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import authOptions  from "../api/auth/[...nextauth]/route";
 
+export default async function Register() {
+  const session = await getServerSession(authOptions);
 
-export default function page() {
-  return <RegisterForm />
+  if (session) redirect("/dashboard");
+
+  return <RegisterForm />;
 }
