56fa512616240a3dab2c3fbc21fdff2f7fe27da1
tlc2880 <tommylcao@gmail.com>
2024-01-12 16:12:25 -0800

    Add dark mode

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -9,12 +9,12 @@ export default function UserInfo() {
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6">
-        <div>
+        <h2>
           Name: <span className="font-bold">{session?.user?.name}</span>
-        </div>
-        <div>
+        </h2>
+        <h2>
           Email: <span className="font-bold">{session?.user?.email}</span>
-        </div>
+        </h2>
         <button
           onClick={() => signOut()}
           className="bg-red-500 text-white font-bold p-6 py-2 mt-3"
